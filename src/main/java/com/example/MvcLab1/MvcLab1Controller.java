3807aa5200fd4ccddd6783b3675757c00e8659de
JHM77 <jill.nyhlen@hm.com>
2021-03-02 13:03:36 +0100

    First commit

---
+++ src/main/java/com/example/MvcLab1/MvcLab1Controller.java
@@ -0,0 +1,13 @@
+package com.example.MvcLab1;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MvcLab1Controller {
+
+    public String test(){
+        return "test";
+    }
+}
+
+
