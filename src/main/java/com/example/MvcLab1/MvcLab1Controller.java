cb99b53ea41cdce79fd25f5e35e439aafea97f66
magsa79 <magnus.gustafsson2@hm.com>
2021-04-16 07:45:26 +0200

    some changes

--- src/main/java/com/example/MvcLab1/MvcLab1Controller.java
+++ src/main/java/com/example/MvcLab1/MvcLab1Controller.java
@@ -19,36 +19,6 @@ public class MvcLab1Controller {
         return "index";
     }
 
-    @GetMapping("/login")
-    public String login() {
-
-        return "login";
-    }
-
-    @PostMapping("/")
-    public String loginPost(HttpSession session, @RequestParam String username, @RequestParam String password) {
-        boolean isLoggedIn = false;
-        if (username.equals("admin") && password.equals("123")) {
-            isLoggedIn = true;
-            session.setAttribute("username", username);
-            session.setAttribute("isLoggedIn", isLoggedIn);
-            return "redirect:/index";
-        }
-        return "login";
-
-    }
-
-    @GetMapping("/index")
-    public String session(HttpSession session) {
-        String username = (String)session.getAttribute("username");
-        if (username != null) {
-            return "index";
-        }
-        else {
-            return "redirect:/";
-        }
-    }
-
     @GetMapping("/allTasks")
     public String allTasks(Model model) {
         List<Task> allTasks = repository.getListAll();
@@ -81,17 +51,6 @@ public class MvcLab1Controller {
     public String addTask(@RequestParam String description, @RequestParam String comment, @RequestParam String owner, @RequestParam (required = false, defaultValue = "false") Boolean isCompleted) {
         Task newTask = new Task(null, description, comment, owner, isCompleted);
         repository.addTask(newTask);
-
         return "index";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpSession session, HttpServletResponse res){
-        session.removeAttribute("username"); // this would be an ok solution
-        session.removeAttribute("isLoggedIn");
-        Cookie cookie = new Cookie("JSESSIONID", "");
-        cookie.setMaxAge(0);
-        res.addCookie(cookie);
-        return "redirect:/";
     }
 }
